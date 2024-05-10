803fdbb96c12aab8b2c51032fea41d7f8eb12ee1
EmaAnd8 <o46002006@studium.unict.it>
2024-05-11 00:01:37 +0200

    First implementation of signup without frontend checking+first attempt to create a login page3

--- lib/screens/components/login_body.dart
+++ lib/screens/components/login_body.dart
@@ -7,7 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 class LoginBody extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
+
     throw UnimplementedError();
   }
 }
