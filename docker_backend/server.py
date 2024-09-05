b1b2844d8aea2c42179d4b0761d395ed6f30245a
biagiomarra12 <biagio.marra@mail.polimi.it>
2024-09-05 19:42:17 +0200

    Added deploy and withdraw buttons, added choice of default topology, ens only at the moment, added screen showing filtered deployment logs

--- docker_backend/server.py
+++ docker_backend/server.py
@@ -31,8 +31,54 @@ def run_script():
 
         logging.info(f"Command output: {output}")
 
+        exec_id2 = client.api.exec_create(container.id, cmd="cat deployment_results.txt", stdout=True, stderr=True)
 
-        return output
+        # Eseguiamo il comando
+        exec_result2 = client.api.exec_start(exec_id2, stream=True)
+
+        output2 = ''
+        for line in exec_result2:  # Raccogliamo l'output dallo stream
+            output2 += line.decode('utf-8')  # Decodifica il byte stream in stringa
+
+        return output2
+
+    except Exception as e:
+        logging.error(f"Error: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/withdraw', methods=['POST'])
+def withdraw():
+    container_id = request.json.get("container_id")
+
+    if not container_id:
+        return jsonify({"error": "Resource ID was not provided"}), 400
+
+    try:
+        container = client.containers.get(container_id)
+        # Creiamo il comando da eseguire nel container
+        exec_id = client.api.exec_create(container.id, cmd="lsof -i :8545 -t", stdout=True, stderr=True)
+
+        # Eseguiamo il comando
+        exec_result = client.api.exec_start(exec_id, stream=True)
+
+
+
+        output = ''
+        for line in exec_result:  # Raccogliamo l'output dallo stream
+            output += line.decode('utf-8')  # Decodifica il byte stream in stringa
+
+        exec_id2 = client.api.exec_create(container.id, cmd="kill -9 {}".format(output), stdout=True, stderr=True)
+        exec_result2 = client.api.exec_start(exec_id2, stream=True)
+
+        output2 = ''
+        for line in exec_result2:  # Raccogliamo l'output dallo stream
+            output2 += line.decode('utf-8')  # Decodifica il byte stream in stringa
+
+        logging.info(f"Command output: {output2}")
+        print(output2)
+
+
+        return output2
 
     except Exception as e:
         logging.error(f"Error: {str(e)}")
--- lib/screens/components/deploy_body.dart
+++ lib/screens/components/deploy_body.dart
@@ -13,6 +13,7 @@ import 'dart:convert'; // For JSON encoding and decoding
 Provider ServiceProvider = Provider.instance;
 Widget simpleTopology = Container();
 
+
 class DeployBody extends StatefulWidget {
   const DeployBody({super.key});
 
@@ -22,9 +23,13 @@ class DeployBody extends StatefulWidget {
 
 class _DeployState extends State<DeployBody> {
   final _formKey = GlobalKey<FormState>();
+  String? selectedOption; // Allow null for no selection
+  String _dynamicText = "Deploy a new topology..."; // Variabile per il testo dinamico
+
 
   Future<void> _sendRequest() async {
-    final url = Uri.parse('http://localhost:5001/run-script'); // Cambia l'URL se necessario
+    final url = Uri.parse(
+        'http://localhost:5001/run-script'); // Cambia l'URL se necessario
 
     final response = await http.post(
       url,
@@ -33,30 +38,83 @@ class _DeployState extends State<DeployBody> {
       },
       body: jsonEncode(<String, String>{
         'container_id': '12cfd61cb30f', // Sostituisci con l'ID del contenitore
-        'script_command': '/bin/sh -c ./run-deploy.sh' ,
+        'script_command': '/bin/sh -c ./run-deploy.sh',
       }),
     );
 
     if (response.statusCode == 200) {
       // Se il server restituisce una risposta OK, mostra l'output
       final String output = response.body;
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Output: $output')));
-      print(output);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Output: $output')));
+      _updateText(output);
     } else {
       // Se il server non restituisce una risposta OK, mostra un errore
       final String error = response.body;
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error: $error')));
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Error: $error')));
       print(error);
     }
   }
 
+  Future<void> _withdrawRequest() async {
+    final url = Uri.parse(
+        'http://localhost:5001/withdraw'); // Cambia l'URL se necessario
+
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'container_id': '12cfd61cb30f', // Sostituisci con l'ID del contenitore
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // Se il server restituisce una risposta OK, mostra l'output
+      final String output = response.body;
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Output: $output')));
+      _updateText("Deploy a new topology...");
+    } else {
+      // Se il server non restituisce una risposta OK, mostra un errore
+      final String error = response.body;
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Error: $error')));
+      print(error);
+    }
+  }
+
+  void _handleRadioValueChange(String? value) {
+    setState(() {
+      // Toggle selection
+      if (selectedOption == value) {
+        selectedOption = null; // Deselect if already selected
+      } else {
+        selectedOption = value; // Select new option
+      }
+    });
+    print('Selected option: $selectedOption'); // Example action
+  }
+
+  void _updateText(String result) {
+    setState(() {
+      _dynamicText = result;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    String selectedOption = 'Option 1';
-    Size size = MediaQuery.of(context).size; // with this query I get (w,h) of the screen
+    Size size = MediaQuery
+        .of(context)
+        .size;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Deploy Viewer'),
+        centerTitle: true,
         backgroundColor: CupertinoColors.white,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
@@ -70,6 +128,23 @@ class _DeployState extends State<DeployBody> {
           },
         ),
         actions: [
+          TextButton(
+            onPressed: _sendRequest,
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.green, // Colore verde
+              foregroundColor: Colors.white, // Colore del testo
+            ),
+            child: const Text('Deploy'),
+          ),
+          const SizedBox(width: 8), // Spazio tra i pulsanti
+          TextButton(
+            onPressed: _withdrawRequest,
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.red, // Colore rosso
+              foregroundColor: Colors.white, // Colore del testo
+            ),
+            child: const Text('Withdraw'),
+          ),
           PopupMenuButton<String>(
             onSelected: (String item) {
               setState(() {
@@ -83,7 +158,7 @@ class _DeployState extends State<DeployBody> {
                   child: Text('Make the Deploy'),
                   onTap: () async {
                     /*
-                    simpleTopology= await ServiceProvider.CreateNode();
+                    simpleTopology = await ServiceProvider.CreateNode();
                     setState(() {
                       simpleTopology;
                     });
@@ -94,18 +169,20 @@ class _DeployState extends State<DeployBody> {
                   value: '2',
                   child: Text('Edit your Topology'),
                   onTap: () {
-                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) {
-                      return TopologyManagementScreen();
-                    }), (route) => false);
+                    Navigator.pushAndRemoveUntil(context,
+                        MaterialPageRoute(builder: (context) {
+                          return TopologyManagementScreen();
+                        }), (route) => false);
                   },
                 ),
                 PopupMenuItem<String>(
                   value: '3',
                   child: Text('View a Topology'),
                   onTap: () async {
-                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) {
-                      return TopologyViewScreen();
-                    }), (route) => false);
+                    Navigator.pushAndRemoveUntil(context,
+                        MaterialPageRoute(builder: (context) {
+                          return TopologyViewScreen();
+                        }), (route) => false);
                   },
                 ),
               ];
@@ -120,19 +197,105 @@ class _DeployState extends State<DeployBody> {
         color: Colors.white,
         child: SingleChildScrollView(
           child: Column(
-            children: <Widget>[
-              Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  Center(
-                    child: simpleTopology,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(3.0), // Margine esterno
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                  // Bordo nero
+                  borderRadius: BorderRadius.circular(
+                      3), // Arrotondamento angoli
+                ),
+                child: Column(
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Default Topology',
+                        style: TextStyle(
+                          fontSize: 15,
+                          // Più grande per maggiore risalto
+                          fontWeight: FontWeight.bold,
+                          // Grassetto per evidenziare il titolo
+                          color: Colors.black87,
+                          // Colore nero leggermente ammorbidito
+                          letterSpacing: 1.0, // Spaziatura delle lettere per eleganza
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              Radio<String?>(
+                                value: 'ens',
+                                groupValue: selectedOption,
+                                onChanged: _handleRadioValueChange,
+                                toggleable: true,
+                              ),
+                              Text('Ens'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Radio<String?>(
+                                value: 'dydx',
+                                groupValue: selectedOption,
+                                onChanged: _handleRadioValueChange,
+                                toggleable: true,
+                              ),
+                              Text('Dydx'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Radio<String?>(
+                                value: 'dark-forest',
+                                groupValue: selectedOption,
+                                onChanged: _handleRadioValueChange,
+                                toggleable: true,
+                              ),
+                              Text('Dark-forest'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          _dynamicText,
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.greenAccent,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
                   ),
                 ],
-              ),
-              SizedBox(height: 20), // Add some space between widgets
-              ElevatedButton(
-                onPressed: _sendRequest,
-                child: Text('Run Script'),
               ),
             ],
           ),
