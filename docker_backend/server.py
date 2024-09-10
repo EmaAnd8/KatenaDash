3bdfad69a74a51dfd3c5ef9aeb3f61bb17ef976c
biagiomarra12 <biagio.marra@mail.polimi.it>
2024-09-10 20:37:21 +0200

    Log monitoring part added, in the python server

--- docker_backend/server.py
+++ docker_backend/server.py
@@ -2,49 +2,71 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import docker
 import logging
-import yaml
+import time
+import threading
 
 app = Flask(__name__)
 CORS(app)
-
 client = docker.from_env()
+start_time = 0
+
+# Funzione di monitoraggio del log
+def monitor_log(exec_id3, stop_event, timeout, container):
+    global start_time
+    start_time = time.time()
+
+    while not stop_event.is_set():
+        if time.time() - start_time > timeout:
+            print("No new data, stopping monitoring due to inactivity.")
+            try:
+                exec_info = client.api.exec_inspect(exec_id3)
+                if exec_info['Running']:
+                    exec_id2 = client.api.exec_create(container.id, cmd="pkill -f 'tail -f'", stdout=True, stderr=True)
+                    client.api.exec_start(exec_id2, stream=True)
+            except Exception as e:
+                print(f"Error killing exec: {e}")
+            break
+        time.sleep(1)  # Pausa per evitare l'uso eccessivo della CPU
+
 
 @app.route('/run-script', methods=['POST'])
 def run_script():
     container_id = request.json.get("container_id")
     script_command = request.json.get("script_command")
     contentFile = request.json.get("content_yaml")
-
-
-    print(contentFile)
-    #contentFileYaml = yaml.dump(contentFile, default_flow_style=False)
-
-
-    #print(contentFileYaml)
+    global start_time
 
     if not container_id or not script_command:
         return jsonify({"error": "Resource ID or script command was not provided"}), 400
 
     try:
         container = client.containers.get(container_id)
-        print(script_command)
-
-        yaml_command = "printf '{}' > /benchmark/file_to_run.yaml".format(contentFile)
-        exec_id_yaml = client.api.exec_create(container.id, cmd=['/bin/sh', '-c', yaml_command], stdout=True, stderr=True)
-        exec_result_yaml = client.api.exec_start(exec_id_yaml, stream=True)
-
-        yaml_output = ''
-        for line in exec_result_yaml:
-            yaml_output += line.decode('utf-8')
-
-        logging.info(f"YAML write output: {yaml_output}")
-
-
-
-        # Creiamo il comando da eseguire nel container
         exec_id = client.api.exec_create(container.id, cmd=script_command, stdout=True, stderr=True)
-        # Eseguiamo il comando
         exec_result = client.api.exec_start(exec_id, stream=True)
+
+        # Monitoraggio del file di log
+        command = "tail -f deploy.log"
+        exec_id3 = client.api.exec_create(container.id, cmd=['/bin/sh', '-c', command], stdout=True, stderr=True)
+        exec_result3 = client.api.exec_start(exec_id3, stream=True)
+
+        stop_event = threading.Event()
+        timeout = 15  # Timeout in secondi
+        monitor_thread = threading.Thread(target=monitor_log, args=(exec_id3, stop_event, timeout, container))
+        monitor_thread.start()
+
+        try:
+            for line in exec_result3:
+                decoded_line = line.decode('utf-8').strip()
+                print(decoded_line)
+                start_time = time.time()  # Assicurati che il timer venga aggiornato correttamente
+
+        except Exception as e:
+            print(f"Error during file monitoring: {e}")
+
+
+        stop_event.set()
+        monitor_thread.join()  # Aspetta che il thread termini
+
 
         output = ''
         for line in exec_result:  # Raccogliamo l'output dallo stream
@@ -53,8 +75,6 @@ def run_script():
         logging.info(f"Command output: {output}")
 
         exec_id2 = client.api.exec_create(container.id, cmd="cat deployment_results.txt", stdout=True, stderr=True)
-
-        # Eseguiamo il comando
         exec_result2 = client.api.exec_start(exec_id2, stream=True)
 
         output2 = ''
@@ -66,7 +86,6 @@ def run_script():
     except Exception as e:
         logging.error(f"Error: {str(e)}")
         return jsonify({"error": str(e)}), 500
-
 
 @app.route('/withdraw', methods=['POST'])
 def withdraw():
@@ -77,28 +96,22 @@ def withdraw():
 
     try:
         container = client.containers.get(container_id)
-        # Creiamo il comando da eseguire nel container
         exec_id = client.api.exec_create(container.id, cmd="lsof -i :8545 -t", stdout=True, stderr=True)
-
-        # Eseguiamo il comando
         exec_result = client.api.exec_start(exec_id, stream=True)
-
-
 
         output = ''
         for line in exec_result:  # Raccogliamo l'output dallo stream
-            output += line.decode('utf-8')  # Decodifica il byte stream in stringa
+            output += line.decode('utf-8').strip()  # Decodifica il byte stream in stringa
 
-        exec_id2 = client.api.exec_create(container.id, cmd="kill -9 {}".format(output), stdout=True, stderr=True)
+        exec_id2 = client.api.exec_create(container.id, cmd=f"kill -9 {output}", stdout=True, stderr=True)
         exec_result2 = client.api.exec_start(exec_id2, stream=True)
 
         output2 = ''
         for line in exec_result2:  # Raccogliamo l'output dallo stream
-            output2 += line.decode('utf-8')  # Decodifica il byte stream in stringa
+            output2 += line.decode('utf-8').strip()  # Decodifica il byte stream in stringa
 
         logging.info(f"Command output: {output2}")
         print(output2)
-
 
         return output2
 
@@ -108,3 +121,4 @@ def withdraw():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
+
